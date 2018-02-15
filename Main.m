3c3f2ba2e7f10af0bd79159b0ce55b5bea2c1f64
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-15 22:25:31 +0530

    Implemented feature selection algorithms via feast

--- Main.m
+++ Main.m
@@ -8,5 +8,21 @@ for z = 1:32
     elabel(count,1)= getEmotion(labels(ceil(count/32),:));
     count = count+1;
 end
-[w,r]= relieff(ARRAY,elabel,4);
 end
+ARRAY = round(Normalize(ARRAY(:,1:17)).*100);
+[w,r]= relieff(ARRAY,elabel,4);
+w(1,1:8);
+[selectedFeatures] = feast('relief',8,ARRAY,elabel);
+[selectedFeatures2] = feast('mim',8,ARRAY,elabel);
+[selectedFeatures3] = feast('mrmr',8,ARRAY,elabel);
+[selectedFeatures4] = feast('cmim',8,ARRAY,elabel);
+[selectedFeatures5] = feast('jmi',8,ARRAY,elabel);
+[selectedFeatures6] = feast('disr',8,ARRAY,elabel);
+[selectedFeatures7] = feast('cife',8,ARRAY,elabel);
+[selectedFeatures8] = feast('icap',8,ARRAY,elabel);
+[selectedFeatures9] = feast('condred',8,ARRAY,elabel);
+% [selectedFeatures10] = feast('cmi',8,ARRAY,elabel);
+d5 = [w(1,1:8)';selectedFeatures;selectedFeatures2;selectedFeatures3;selectedFeatures4;selectedFeatures5;selectedFeatures6;selectedFeatures7;selectedFeatures8;selectedFeatures9;];
+% 
+% % where algName is:
+% %  mim, mrmr, cmim, jmi, disr, cife, icap, condred, cmi, relief
