ecf6967b7762153ad1387bbbf2d9827ecd18707d
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-15 22:26:09 +0530

    Minor fixes

--- Normalize.m
+++ Normalize.m
@@ -2,10 +2,10 @@ function [ Normalized ] = Normalize( input )
 %NORMALIZE Summary of this function goes here
 %   Detailed explanation goes here
 Normalized = zeros(size(input));
-mi = min(input)
-ma = max(input)
-ra = ma-mi
-for i=1:size(input,1)
-Normalized(i,:) = (input(i,:)-mi)./ra
+mi = min(input);
+ma = max(input);
+ra = ma-mi;
+for i=1:size(input,1);
+Normalized(i,:) = (input(i,:)-mi)./ra;
 end
 
