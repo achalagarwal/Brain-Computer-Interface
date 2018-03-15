bad901b5f0338669fa140ec06d3cee8d9fd529ba
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-15 09:41:19 +0530

    Added 2 new features. Storing best features for each human

--- Main.m
+++ Main.m
@@ -9,10 +9,9 @@ for z = 1:32
     count = count+1;
 end
 end
-ARRAY = round(Normalize(ARRAY(:,1:17)).*100);
+ARRAY = round(Normalize(ARRAY(:,1:19)).*100);
 [w,r]= relieff(ARRAY,elabel,4);
-w(1,1:8);
-[selectedFeatures] = feast('relief',8,ARRAY,elabel);
+w = w(1,1:8);
 [selectedFeatures2] = feast('mim',8,ARRAY,elabel);
 [selectedFeatures3] = feast('mrmr',8,ARRAY,elabel);
 [selectedFeatures4] = feast('cmim',8,ARRAY,elabel);
@@ -22,7 +21,9 @@ w(1,1:8);
 [selectedFeatures8] = feast('icap',8,ARRAY,elabel);
 [selectedFeatures9] = feast('condred',8,ARRAY,elabel);
 % [selectedFeatures10] = feast('cmi',8,ARRAY,elabel);
-d5 = [w(1,1:8)';selectedFeatures;selectedFeatures2;selectedFeatures3;selectedFeatures4;selectedFeatures5;selectedFeatures6;selectedFeatures7;selectedFeatures8;selectedFeatures9;];
+d5 = [w',selectedFeatures2,selectedFeatures3,selectedFeatures4,selectedFeatures5,selectedFeatures6,selectedFeatures7,selectedFeatures8,selectedFeatures9];
+trie = reshape(d5,8,9);
+c10= analyse(trie);
 % 
 % % where algName is:
 % %  mim, mrmr, cmim, jmi, disr, cife, icap, condred, cmi, relief
